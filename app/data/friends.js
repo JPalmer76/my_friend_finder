4603cb9623f253160c7de462f89fba2e27d20cdf
Jonny <jpdadof3@gmail.com>
2019-07-27 23:25:02 -0500

    testing routing and running in the browser

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,53 @@
 var friends = [
     {
-        name: "",
-        photo: "",
-        scores:[]
+        name: "Leonardo DiCaprio",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Leonardo_DiCaprio_visited_Goddard_Saturday_to_discuss_Earth_science_with_Piers_Sellers_%2826105091624%29_cropped.jpg/800px-Leonardo_DiCaprio_visited_Goddard_Saturday_to_discuss_Earth_science_with_Piers_Sellers_%2826105091624%29_cropped.jpg",
+        scores:[1,2,5,5,1,3,3,4,1,1]
+    },
+    {
+        name: "Amy Poehler",
+        photo: "https://cdn.vox-cdn.com/thumbor/GJcqzYPcjDg1JR-zloVsAf3Ow2s=/7x0:1490x1112/920x613/filters:focal(7x0:1490x1112):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/45684302/amy-poehler-lead_10_2014.0.0.0.jpg",
+        scores:[3,2,1,1,4,2,4,4,1,3]
+    },
+    {
+        name: "Brad Pitt",
+        photo: "http://www.delabahia.com.ar/wp-content/uploads/2017/10/brad-pitt-11.jpg",
+        scores:[4,2,2,5,1,1,1,3,4,4]
+    },
+    {
+        name: "Rashida Jones",
+        photo: "https://www.indiewire.com/wp-content/uploads/2017/11/screen-shot-2017-11-22-at-8-31-23-am.png",
+        scores:[3,3,3,3,4,1,5,1,1,5]
+    },
+    {
+        name: "John Krasinski",
+        photo: "https://vignette.wikia.nocookie.net/theoffice/images/8/80/John_Krasinski1.jpg/revision/latest?cb=20171103160323",
+        scores:[2,4,4,2,1,4,4,2,3,5]
+    },
+    {
+        name: "Aubrey Plaza",
+        photo: "https://hips.hearstapps.com/esq.h-cdn.co/assets/cm/15/06/54d4627f5285c_-_esq-aubrey-de.jpg?resize=768:*",
+        scores:[4,4,4,4,2,2,1,4,3,1]
+    },
+    {
+        name: "Matt Damon",
+        photo: "https://timedotcom.files.wordpress.com/2015/09/matt-damon3.jpg",
+        scores:[1,1,1,4,2,4,2,3,1,1]
+    },
+    {
+        name: "Tina Fey",
+        photo: "https://s1.r29static.com//bin/entry/eac/340x408,85/1962536/image.webp",
+        scores:[3,3,3,3,3,2,4,1,1,3]
+    },
+    {
+        name: "Chris Pratt",
+        photo: "https://www.indiewire.com/wp-content/uploads/2018/05/shutterstock_9640572ek.jpg?resize=900,600",
+        scores:[4,4,4,4,4,2,1,2,3,1]
+    },
+    {
+        name: "Jenna Fischer",
+        photo: "https://bookingagentinfo.com/wp-content/uploads/2018/02/Jenna-Fischer-Contact-Information.jpg",
+        scores:[5,5,5,2,2,1,3,4,1,1]
     }
 ];
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require("express");
-var bodyParser = ("body-parser");
+// var bodyParser = ("body-parser");
 var path = require("path");
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: ""}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.static("app/public"));
+app.use(express.static("app/routing"));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(port, () => console.log("Listening on port %s", port));
+app.listen(port, function() {
+    console.log("App listening on PORT " + port);
+});
