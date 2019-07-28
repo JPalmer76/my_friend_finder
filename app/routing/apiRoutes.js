23dfd3d9ac2d2a350e48baa86b7beb07e9651b09
Jonny <jpdadof3@gmail.com>
2019-07-27 23:53:55 -0500

    can't get routing to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
-var friends = require("../data/friends.js");
+var friends = require("app/data/friends.js");
 
 module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
+  app.get("app/data/friends.js", function(req, res) {
     res.json(friends);
   });
 
