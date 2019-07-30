9531de2823ec68f8e3f102ceffd0ee797e39235a
Jonny <jpdadof3@gmail.com>
2019-07-29 20:06:40 -0500

    fixed broken paths

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
-var friends = require("app/data/friends.js");
+var friends = require("../data/friends");
 
 module.exports = function(app) {
-  app.get("app/data/friends.js", function(req, res) {
+  app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
@@ -30,7 +30,7 @@ module.exports = function(app) {
     console.log("Name: " + userName);
     console.log("User Score " + userScores);
 
-    var sum = b.reduce((a, b) => a + b, 0);
+    var sum = b.reduce(a, b, 0);
 
     console.log("Sum of users score " + sum);
     console.log("Best match friend diff " + bestMatch.friendDifference);
@@ -42,7 +42,7 @@ module.exports = function(app) {
       console.log("Total Diff " + totalDifference);
       console.log("Best match friend diff " + bestMatch.friendDifference);
 
-      var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+      var bfriendScore = friends[i].scores.reduce(a, b, 0);
       console.log("Total friend score " + bfriendScore);
       totalDifference += Math.abs(sum - bfriendScore);
       console.log("-------------------------> " + totalDifference);
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var express = require("express");
 // var bodyParser = ("body-parser");
+var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
 
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("app/routing"));
+app.use(express.static("app/public"));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
